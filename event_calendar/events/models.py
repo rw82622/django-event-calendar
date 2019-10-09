df06ee1f44851f2a45221a0e610908e7cfbf4bbb
Mic-J-Lee <mleenavy@gmail.com>
2019-10-09 16:17:41 -0500

    PEP8 update

--- event_calendar/events/models.py
+++ event_calendar/events/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from django.utils import timezone 
 
 class Event(models.Model):
-  name = models.CharField(max_length=200)
-  description = models.TextField()
-  starts_at = models.DateTimeField(default=timezone.now)
-  ends_at = models.DateTimeField(default=timezone.now)
-  
-  created_at = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    starts_at = models.DateTimeField(default=timezone.now)
+    ends_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
